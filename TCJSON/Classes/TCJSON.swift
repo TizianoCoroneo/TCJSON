795766ab08e038f38e01db626d7b5a0e56673286
Tiziano Coroneo <tizianocoroneo@me.com>
2017-12-21 11:33:37 +0100

    Commented everything and provided cocoapods the target platform.

--- TCJSON/Classes/TCJSON.swift
+++ TCJSON/Classes/TCJSON.swift
@@ -7,35 +7,58 @@
 
 import Foundation
 
-public struct TCJSON<Content: Codable> {
+/// Utility wrapper for common `Codable` operations.
+public struct TCJSON<Content: Codable>: Codable {
     private let _content: Content?
     private let _json: Data?
     
+    /// Returns the model object.
+    ///
+    /// - Returns: The model object used to initialize this instance, or decoded from `Data`.
+    /// - Throws: Rethrows from `JSONDecoder`.
     public func content() throws -> Content {
         if let content = _content { return content }
         return try JSONDecoder().decode(Content.self, from: _json!)
     }
     
+    /// Returns `Data` corresponding to the content object encoded with `JSONEncoder`.
+    ///
+    /// - Returns: The `Data` object.
+    /// - Throws: Rethrows from `JSONEncoder`.
     public func data() throws -> Data {
         if let json = _json { return json }
         return try JSONEncoder().encode(_content!)
     }
     
+    /// Initialize from a model object that conforms to Codable.
+    ///
+    /// - Parameter content: Model object.
     public init(_ content: Content) {
         self._content = content
         self._json = nil
     }
     
+    /// Initialize from a `JSONEncoded` `Data` object.
+    ///
+    /// - Parameter json: The input `Data`.
     public init(_ json: Data) {
         self._json = json
         self._content = nil
     }
     
-    public init(jsonString: String) throws {
+    /// Initialize from a valid JSON formatted string. If the string is not valid, the exception will be thrown only when asking for `content()`.
+    ///
+    /// - Parameter jsonString: A string that contains valid JSON content.
+    public init(jsonString: String) {
         let data = jsonString.data(using: .utf8)!
         self.init(data)
     }
     
+    /// Do something with the JSON formatted as `Data`, and return a new `TCJSON` object.
+    ///
+    /// - Parameter f: The function gets `Data` and return a new `Content` for the new `TCJSON` object.
+    /// - Returns: The new `TCJSON` object.
+    /// - Throws: Rethrows from the application of `f` and the conversion in `Data` of the content.
     public func map<X>(
         data f: (Data) throws -> X)
         throws -> TCJSON<X> {
@@ -43,6 +66,11 @@ public struct TCJSON<Content: Codable> {
             return TCJSON<X>(try f(try data()))
     }
     
+    /// Do something with the JSON formatted as `Content`, and return a new `TCJSON` object.
+    ///
+    /// - Parameter f: The function gets `Content` and return a new `Content` for the new `TCJSON` object.
+    /// - Returns: The new `TCJSON` object.
+    /// - Throws: Rethrows from the application of `f` and the conversion in `Content` of the content.
     public func map<X>(
         content f: (Content) throws -> X)
         throws -> TCJSON<X> {
@@ -50,40 +78,89 @@ public struct TCJSON<Content: Codable> {
             return TCJSON<X>(try f(try content()))
     }
     
+    /// Do something with the JSON formatted as `Data`, and return a new `TCJSON` object.
+    ///
+    /// - Parameter f: The function gets `Data` and return a new `TCJSON` object.
+    /// - Returns: The new `TCJSON` object returned by f.
+    /// - Throws: Rethrows from the mapping of `f`, the conversion in `Data` of the content, and the flattening operation.
     public func bind<X>(
         data f: (Data) throws -> TCJSON<X>)
         throws -> TCJSON<X> {
-            
-            return try f(try data())
+            return try map(data: f).flatten()
+//            return try f(try data())
     }
     
+    /// Do something with the JSON formatted as `Content`, and return a new `TCJSON` object.
+    ///
+    /// - Parameter f: The function gets `Content` and return a new `TCJSON` object.
+    /// - Returns: The new `TCJSON` object returned by f.
+    /// - Throws: Rethrows from the mapping of `f`, the conversion in `Content` of the content, and the flattening operation.
     public func bind<X>(
         content f: (Content) throws -> TCJSON<X>)
         throws -> TCJSON<X> {
-            
-            return try f(try content())
+            return try map(content: f).flatten()
+//            return try f(try content())
     }
     
+    /// If the current `TCJSON` contains another `TCJSON` object (like `TCJSON<TCJSON<String>>`), get rid of the outer object and returns the internal one (like `(TCJSON<TCJSON<String>>) -> TCJSON<String>`).
+    ///
+    /// - Returns: The new internal TCJSON object.
+    /// - Throws: Can throw `FlatteningError` if the swift type infer system fucks up. It shouldn't be possible to throw it.
+    public func flatten<X: Codable>() throws -> TCJSON<X> {
+        let content = try self.content()
+        
+        if let c = content as? TCJSON<X> { return c }
+        if let s = self as? TCJSON<X> { return s }
+        
+        throw FlatteningError(fromType: Content.self, toType: X.self)
+    }
+    
+    /// Applies a function to the `Data` content of a `TCJSON` object.
+    ///
+    /// - Parameter f: Function to be applied on `Data`.
+    /// - Throws: Rethrows from the function application and the conversion to `Data`.
     public func apply(
         data f: (Data) throws -> ())
         throws {
-            
             try f(try data())
     }
     
+    /// Applies a function to the `Content` of a `TCJSON` object.
+    ///
+    /// - Parameter f: Function to be applied on `Content`.
+    /// - Throws: Rethrows from the function application and the conversion to `Content`.
     public func apply(
         content f: (Content) throws -> ())
         throws {
-            
             try f(try content())
     }
     
+    /// Takes the content of two `TCJSON` objects, applies a function to them and creates a new TCJSON object from the result.
+    ///
+    /// - Parameters:
+    ///   - x: `TCJSON` that provides the first argument of the function.
+    ///   - y: `TCJSON` that provides the second argument of the function.
+    ///   - closure: Function to be applied to the contents of the two `TCJSON`s.
+    /// - Returns: The new `TCJSON` object with the content of the result of `f`.
+    /// - Throws: Rethrows from initializing with content and from the two conversions to content.
     public static func zip<X, Y, Z: Codable>(
         x: TCJSON<X>,
         y: TCJSON<Y>,
         closure: (X, Y) throws -> Z)
         throws -> TCJSON<Z> {
-            
             return try TCJSON<Z>(closure(try x.content(), try y.content()))
+    }
+    
+    /// This error could occur when flattening a TCJSON which doesn't need flattening.
+    struct FlatteningError<X, Y>: Error {
+        var localizedDescription: String {
+            return string
+        }
+        
+        private let string: String
+        
+        init(fromType from: X.Type, toType to: Y.Type) {
+            self.string = "Impossible casting from \(from) to \(to)"
+        }
     }
 }
--- TCJSON/Classes/TCJSONCodable.swift
+++ TCJSON/Classes/TCJSONCodable.swift
@@ -7,24 +7,45 @@
 
 import Foundation
 
+/// `TCJSONCodable` extends `Codable` and provides access to the correpsoding `TCJSON` object and two utility initializers: `init(fromData:)` and `init(fromJSONString:)`
 public protocol TCJSONCodable: Codable {
+    /// The TCJSON object. Every `TCJSONCodable` gets one.
     var json: TCJSON<Self> { get }
     
+    /// Initialize the model object from `Data` that represents it, using `JSONDecoder`.
+    ///
+    /// - Parameter data: The `Data` object.
+    /// - Throws: Rethrows from the `JSONDecoder` `decode` method.
     init(fromData data: Data) throws
+    
+    /// Initialize the model object from a `String` that represents it, using `JSONDecoder` after encoding the string in utf8.
+    ///
+    /// - Parameter data: The JSON formatted `String`.
+    /// - Throws: Rethrows from the `JSONDecoder` `decode` method.
     init(fromJSONString string: String) throws
 }
 
+// MARK: - Default implementations of `TCJSONCodable`
 extension TCJSONCodable {
+    /// Just initialize it from the content of the model.
     public var json: TCJSON<Self> {
         return TCJSON<Self>(self)
     }
     
+    /// Initialize a `TCJSON` object from `Data` and initialize `self` with it.
+    ///
+    /// - Parameter data: The `Data` object.
+    /// - Throws: Rethrows from the TCJSON conversion from `Data` to `Content`
     public init(fromData data: Data) throws {
         self = try TCJSON(data).content()
     }
 
+    /// Initialize a `TCJSON` object from a `String` and initialize `self` with it.
+    ///
+    /// - Parameter data: The JSON formatted `String`.
+    /// - Throws: Rethrows from the TCJSON conversion from `Data` to `Content`
     public init(fromJSONString string: String) throws {
-        let json = try TCJSON<Self>(jsonString: string)
+        let json = TCJSON<Self>(jsonString: string)
         self = try json.content()
     }
 }

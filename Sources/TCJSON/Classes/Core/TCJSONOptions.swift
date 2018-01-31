541a6274a14c871dbfddb0ac8df61b44b0d42046
Tiziano Coroneo <tizianocoroneo@me.com>
2018-01-31 13:09:48 +0100

    Made TCJSONOptions properties public.

--- Sources/TCJSON/Classes/Core/TCJSONOptions.swift
+++ Sources/TCJSON/Classes/Core/TCJSONOptions.swift
@@ -10,9 +10,9 @@ import Foundation
 public struct TCJSONOptions {
 
     /// Encoder and Decoder couple used by `TCJSON` when transforming the data in other forms. Defaults to `JSONEncoder`.
-    static var defaultEncoder = JSONEncoder()
-    static var defaultDecoder = JSONDecoder()
+    public static var defaultEncoder = JSONEncoder()
+    public static var defaultDecoder = JSONDecoder()
 
     /// Set this to `true` if you want TCJSON to log every data conversion.
-    static var isVerbose: Bool = false
+    public static var isVerbose: Bool = false
 }
